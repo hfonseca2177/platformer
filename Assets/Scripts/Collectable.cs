973fdd409f1d18853ef11150b4c41b40dfee0bcf
hfonseca2177 <hfonseca21@gmail.com>
2020-12-02 18:19:54 -0300

    first commit

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType {  Coin, Ammo, Health }
+
+public class Collectable : MonoBehaviour
+{
+
+    [SerializeField] private ItemType itemType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Colliding something...");
+
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("Collision with a " + itemType);
+            collision.gameObject.GetComponent<NewPlayer>().coins += 1;
+            Destroy(gameObject);
+        }
+    }
+    
+}
---
+++ Assets/Scripts/NewPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPlayer : PhysicsObject
+{
+    const string HorizontalKey = "Horizontal";
+    const string JumpKey = "Jump";
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    public int coins;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        targetVelocity = new Vector2(Input.GetAxis(HorizontalKey) * speed ,0);
+        if(Input.GetButton(JumpKey) && grounded)
+        {
+            velocity.y = jumpForce;
+        }        
+    }
+}
